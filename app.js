4ed01e8841d6cc7eb34f17f94e29d5ab24be1fbe
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2022-10-25 22:25:44 -0300

    site de filmes

---
+++ app.js
@@ -0,0 +1,13 @@
+let filme01= 'imagens/filme01.jpg'
+let filme02= 'imagens/filme02.jpg'
+let filme03= 'imagens/filme03.jpg'
+let filme04= 'imagens/bem vindos.jpg'
+let filme05= 'imagens/continencia.jpg'
+let filme06='imagens/maravilha.jpg'
+document.querySelector(".container").innerHTML+= "<img src=' " + filme01 +"'>           "
+document.querySelector(".container").innerHTML+= "<img src=' " + filme02 +"'>           "
+document.querySelector(".container").innerHTML+= "<img src=' " + filme03 +"'>           "
+document.querySelector(".container").innerHTML+= "<img src=' " + filme04 +"'>           "
+document.querySelector(".container").innerHTML+= "<img src=' " + filme05 +"'>           "
+document.querySelector(".container").innerHTML+= "<img src=' " + filme06 +"'>           "
+
